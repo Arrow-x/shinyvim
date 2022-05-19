0f3d7ea8421e7112c840f42758cb80fadc7444df
Arrow-x <Houssem.kun26@gmail.com>
2022-05-19 03:13:15 +0100

    ascii art

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -5,12 +5,12 @@ end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+	[[   _____  __     _               _    __ _          ]],
+	[[  / ___/ / /_   (_)____   __  __| |  / /(_)____ ___ ]],
+	[[  \__ \ / __ \ / // __ \ / / / /| | / // // __ `__ \]],
+	[[ ___/ // / / // // / / // /_/ / | |/ // // / / / / /]],
+	[[/____//_/ /_//_//_/ /_/ \__, /  |___//_//_/ /_/ /_/ ]],
+	[[                       /____/                       ]],
 }
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "Arrow-x"
+	return "https://github.com/Arrow-x/shinyvim"
 end
 
 dashboard.section.footer.val = footer()
