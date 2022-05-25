c933be15ef475c64f96b677aa5048e602ee9dec3
Arrow-x <Houssem.kun26@gmail.com>
2022-05-25 17:59:03 +0100

    auto set-up all the servers that lsp intaller have

--- lua/user/lsp/config.lua
+++ lua/user/lsp/config.lua
@@ -1,31 +1,36 @@
-local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
 	return
 end
 
-local lspconfig = require("lspconfig")
+local insert = table.insert
+local tbl_deep_extend = vim.tbl_deep_extend
+local handlers = require("user.lsp.handlers")
+local on_attach = handlers.on_attach
 
-local servers = {
-	"sumneko_lua",
-	"rust_analyzer",
-	-- "pyright",
-	-- "jedi_language_server",
-	"pylsp",
-}
+local servers = {}
+local installer_avail, lsp_installer = pcall(require, "nvim-lsp-installer")
+if installer_avail then
+	for _, server in ipairs(lsp_installer.get_installed_servers()) do
+		insert(servers, server.name)
+	end
+	lsp_installer.setup()
+end
 
-lsp_installer.setup({
-	ensure_installed = servers,
-	automatic_installation = false,
-})
-
-for _, server in pairs(servers) do
+for _, server in ipairs(servers) do
+	local old_on_attach = lspconfig[server].on_attach
 	local opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
+		on_attach = function(client, bufnr)
+			if old_on_attach then
+				old_on_attach(client, bufnr)
+			end
+			on_attach(client, bufnr)
+		end,
+		capabilities = tbl_deep_extend("force", handlers.capabilities, lspconfig[server].capabilities or {}),
 	}
-	local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
-	if has_custom_opts then
-		opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+	local present, av_overrides = pcall(require, "user.lsp.settings." .. server)
+	if present then
+		opts = tbl_deep_extend("force", av_overrides, opts)
 	end
 	lspconfig[server].setup(opts)
 end
