4f34dd5ce8a008c7e0c25ee7afb10b26ebc0e47e
Arrow-x <Houssem.kun26@gmail.com>
2022-06-16 15:46:05 +0100

    treesitter useless stuff

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -7,9 +7,9 @@ configs.setup({
 	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 	ignore_install = { "" }, -- List of parsers to ignore installing
-	autopairs = {
-		enable = true,
-	},
+	-- autopairs = {
+	-- 	enable = true,
+	-- },
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "" }, -- list of language that will be disabled
@@ -23,9 +23,9 @@ configs.setup({
 		-- colors = {}, -- table of hex strings
 		-- termcolors = {} -- table of colour name strings
 	},
-	indent = { enable = true, disable = { "yaml" } },
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
+	-- indent = { enable = true, disable = { "yaml" } },
+	-- context_commentstring = {
+	-- 	enable = true,
+	-- 	enable_autocmd = false,
+	-- },
 })
