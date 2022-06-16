05d317ca434f83b24c4fb302c792bab453f9e75f
Arrow-x <Houssem.kun26@gmail.com>
2022-06-16 19:18:58 +0100

    no autocommand groups

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,43 +1,40 @@
-local groups = {
-	general_settings = vim.api.nvim_create_augroup("general_settings", { clear = true }),
-	git = vim.api.nvim_create_augroup("git", { clear = true }),
-	markdown = vim.api.nvim_create_augroup("markdown", { clear = true }),
-	mail = vim.api.nvim_create_augroup("mail", { clear = true }),
-	auto_resize = vim.api.nvim_create_augroup("auto_resize", { clear = true }),
-	alpha = vim.api.nvim_create_augroup("alpha", { clear = true }),
-	lsp = vim.api.nvim_create_augroup("lsp", { clear = true }),
-	exe_code = vim.api.nvim_create_augroup("exe_code", { clear = true }),
-}
+-- local groups = {
+-- 	general_settings = vim.api.nvim_create_augroup("general_settings", { clear = true }),
+-- 	git = vim.api.nvim_create_augroup("git", { clear = true }),
+-- 	markdown = vim.api.nvim_create_augroup("markdown", { clear = true }),
+-- 	mail = vim.api.nvim_create_augroup("mail", { clear = true }),
+-- 	auto_resize = vim.api.nvim_create_augroup("auto_resize", { clear = true }),
+-- 	alpha = vim.api.nvim_create_augroup("alpha", { clear = true }),
+-- 	lsp = vim.api.nvim_create_augroup("lsp", { clear = true }),
+-- 	exe_code = vim.api.nvim_create_augroup("exe_code", { clear = true }),
+-- }
 
-local c_autocmd = vim.api.nvim_create_autocmd
+local acmd = vim.api.nvim_create_autocmd
 
-c_autocmd("TextYankPost", {
-	group = groups.general_settings,
+acmd("TextYankPost", {
 	pattern = "*",
 	callback = function()
 		require("vim.highlight").on_yank({ higroup = "Visual", timeout = 200 })
 	end,
 })
 
-c_autocmd("FileType", {
-	group = groups.general_settings,
+acmd("FileType", {
 	pattern = "qf",
 	callback = function()
 		vim.cmd("set nobuflisted")
 	end,
 })
 
-c_autocmd({ "BufWinEnter" }, {
-	group = groups.general_settings,
+acmd({ "BufWinEnter" }, {
 	pattern = "*",
 	callback = function()
+		-- vim.opt.formatoptions:remove("cro")
 		vim.cmd("set formatoptions-=cro")
 	end,
 })
 
-c_autocmd("FileType", {
-	group = groups.general_settings,
-	pattern = "qf,help,lspinfo",
+acmd("FileType", {
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
 	callback = function()
 		vim.keymap.set("n", "q", function()
 			vim.cmd("close")
@@ -45,57 +42,35 @@ c_autocmd("FileType", {
 	end,
 })
 
-c_autocmd("BufWritePre", {
-	group = groups.lsp,
+acmd("BufWritePre", {
 	pattern = "*",
 	callback = function()
 		vim.lsp.buf.formatting_sync()
 	end,
 })
 
-c_autocmd("FileType", {
-	group = groups.git,
-	pattern = "gitcommit",
+acmd("FileType", {
+	pattern = { "gitcommit", "mail", "markdown" },
 	callback = function()
 		vim.wo.spell = true
 		vim.wo.wrap = true
 	end,
 })
 
-c_autocmd("FileType", {
-	group = groups.mail,
-	pattern = "mail",
-	callback = function()
-		vim.wo.spell = true
-		vim.wo.wrap = true
-	end,
-})
-
-c_autocmd("FileType", {
-	group = groups.markdown,
-	pattern = "markdown",
-	callback = function()
-		vim.wo.spell = true
-		vim.wo.wrap = true
-	end,
-})
-c_autocmd("User", {
-	group = groups.alpha,
+acmd("User", {
 	pattern = "AlphaReady",
 	callback = function()
 		vim.cmd("set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2")
 	end,
 })
-c_autocmd("VimResized", {
-	group = groups.auto_resize,
+acmd("VimResized", {
 	pattern = "*",
 	callback = function()
 		vim.cmd("tabdo wincmd =")
 	end,
 })
 
-c_autocmd("FileType", {
-	group = groups.exe_code,
+acmd("FileType", {
 	pattern = "python",
 	callback = function()
 		vim.keymap.set(
@@ -108,8 +83,7 @@ c_autocmd("FileType", {
 	end,
 })
 
-c_autocmd("FileType", {
-	group = groups.exe_code,
+acmd("FileType", {
 	pattern = "bash, sh",
 	callback = function()
 		vim.keymap.set(
