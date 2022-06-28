b6a13ad4144d055904427a373a3a123590f6e44d
Arrow-x <Houssem.kun26@gmail.com>
2022-06-28 13:28:46 +0100

    vim wiki better markdown link generation

--- lua/user/vimwiki.lua
+++ lua/user/vimwiki.lua
@@ -1,1 +1,4 @@
-vim.cmd("let g:vimwiki_list = [{'path': '~/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]")
+vim.cmd(
+	"let g:vimwiki_list = [{ 'path': '~/vimwiki', 'template_path': '~/vimwiki/templates/', 'template_default': 'default', 'syntax': 'markdown', 'ext': '.md', 'path_html': '~/vimwiki/site_html/', 'custom_wiki2html': 'vimwiki_markdown', 'template_ext': '.tpl', 'links_space_char': '_'}]"
+)
+vim.cmd("let g:vimwiki_markdown_link_ext = 1")
