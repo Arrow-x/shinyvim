b5c64786beb4665d7531ea39b1fc266c30f834ae
Arrow-x <Houssem.kun26@gmail.com>
2022-07-06 10:20:17 +0100

    use lua functions when available in keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -125,121 +125,183 @@ keymap("v", "<leader>p", '"+p', opts)
 keymap("v", "<leader>P", '"+P', opts)
 
 -- Lsp keymaps
-if available("lspconfig") then
-	keymap("n", "<leader>ld", "<cmd>lua vim.lsp.buf.definition()<CR>", { silent = true, desc = "go to Definition" })
-	keymap("n", "<leader>lc", "<cmd>lua vim.lsp.buf.declaration()<CR>", { silent = true, desc = "go to Declaration" })
-	keymap(
-		"n",
-		"<leader>li",
-		"<cmd>lua vim.lsp.buf.implementation()<CR>",
-		{ silent = true, desc = "go to Implementation" }
-	)
-	keymap("n", "<leader>lR", "<cmd>lua vim.lsp.buf.references()<CR>", { silent = true, desc = "go to references" })
-	keymap("n", "<leader>lH", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { silent = true, desc = "Signature Help" })
-	keymap("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<CR>", { silent = true, desc = "Rename" })
-	keymap("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", { silent = true, desc = "Code Actions" })
-	keymap(
-		"n",
-		"<leader>ln",
-		'<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>',
-		{ silent = true, desc = "Go to next Diagnostics" }
-	)
-	keymap(
-		"n",
-		"<leader>lp",
-		'<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>',
-		{ silent = true, desc = "Go to prev Diagnostics" }
-	)
-	keymap("n", "<leader>lh", "<cmd>lua vim.diagnostic.open_float()<CR>", { silent = true, desc = "Hover Diagnostics" })
-	keymap(
-		"n",
-		"<leader>lq",
-		"<cmd>lua vim.diagnostic.setloclist()<CR>",
-		{ silent = true, desc = "Diagnostics to loacal list" }
-	)
-	keymap("n", "<leader>lI", "<cmd>LspInfo<cr>", { silent = true, desc = "Info" })
-	keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<cr>", { silent = true, desc = "Format" })
+if pcall(require, "lspconfig") then
+	keymap("n", "<leader>ld", function()
+		vim.lsp.buf.definition()
+	end, { silent = true, desc = "go to Definition" })
+
+	keymap("n", "<leader>lc", function()
+		vim.lsp.buf.declaration()
+	end, { silent = true, desc = "go to Declaration" })
+
+	keymap("n", "<leader>li", function()
+		vim.lsp.buf.implementation()
+	end, { silent = true, desc = "go to Implementation" })
+
+	keymap("n", "<leader>lR", function()
+		vim.lsp.buf.references()
+	end, { silent = true, desc = "go to references" })
+
+	keymap("n", "<leader>lH", function()
+		vim.lsp.buf.signature_help()
+	end, { silent = true, desc = "Signature Help" })
+
+	keymap("n", "<leader>lr", function()
+		vim.lsp.buf.rename()
+	end, { silent = true, desc = "Rename" })
+
+	keymap("n", "<leader>la", function()
+		vim.lsp.buf.code_action()
+	end, { silent = true, desc = "Code Actions" })
+
+	keymap("n", "<leader>ln", function()
+		vim.diagnostic.goto_next({ border = "rounded" })
+	end, { silent = true, desc = "Go to next Diagnostics" })
+
+	keymap("n", "<leader>lp", function()
+		vim.diagnostic.goto_prev({ border = "rounded" })
+	end, { silent = true, desc = "Go to prev Diagnostics" })
+
+	keymap("n", "<leader>lh", function()
+		vim.diagnostic.open_float()
+	end, { silent = true, desc = "Hover Diagnostics" })
+
+	keymap("n", "<leader>lq", function()
+		vim.diagnostic.setloclist()
+	end, { silent = true, desc = "Diagnostics to loacal list" })
+
+	keymap("n", "<leader>lI", function()
+		vim.cmd("LspInfo")
+	end, { silent = true, desc = "Info" })
+
+	keymap("n", "<leader>lf", function()
+		vim.lsp.buf.formatting()
+	end, { silent = true, desc = "Format" })
 end
 
 -- Git keymaps
-if available("gitsigns") then
-	keymap("n", "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", { silent = true, desc = "Next Hunk" })
-	keymap("n", "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", { silent = true, desc = "Prev Hunk" })
-	keymap("n", "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>", { silent = true, desc = "Blame" })
-	keymap(
-		"n",
-		"<leader>gp",
-		"<cmd>lua require 'gitsigns'.preview_hunk()<cr>",
-		{ silent = true, desc = "Preview Hunk" }
-	)
-	keymap("n", "<leader>gr", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", { silent = true, desc = "Reset Hunk" })
-	keymap(
-		"n",
-		"<leader>gR",
-		"<cmd>lua require 'gitsigns'.reset_buffer()<cr>",
-		{ silent = true, desc = "Reset Buffer" }
-	)
-	keymap("n", "<leader>gs", "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", { silent = true, desc = "Stage Hunk" })
-	keymap(
-		"n",
-		"<leader>gu",
-		"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-		{ silent = true, desc = "Undo Stage Hunk" }
-	)
-	keymap("n", "<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", { silent = true, desc = "Diff" })
+local status_ok, gitsigns = pcall(require, "gitsigns")
+if status_ok then
+	keymap("n", "<leader>gj", function()
+		gitsigns.next_hunk()
+	end, { silent = true, desc = "Next Hunk" })
+	keymap("n", "<leader>gk", function()
+		gitsigns.prev_hunk()
+	end, { silent = true, desc = "Prev Hunk" })
+	keymap("n", "<leader>gl", function()
+		gitsigns.blame_line()
+	end, { silent = true, desc = "Blame" })
+	keymap("n", "<leader>gp", function()
+		gitsigns.preview_hunk()
+	end, { silent = true, desc = "Preview Hunk" })
+	keymap("n", "<leader>gr", function()
+		gitsigns.reset_hunk()
+	end, { silent = true, desc = "Reset Hunk" })
+	keymap("n", "<leader>gR", function()
+		gitsigns.reset_buffer()
+	end, { silent = true, desc = "Reset Buffer" })
+	keymap("n", "<leader>gs", function()
+		gitsigns.stage_hunk()
+	end, { silent = true, desc = "Stage Hunk" })
+	keymap("n", "<leader>gu", function()
+		gitsigns.undo_stage_hunk()
+	end, { silent = true, desc = "Undo Stage Hunk" })
+	keymap("n", "<leader>gd", function()
+		vim.cmd("Gitsigns diffthis HEAD end")
+	end, { silent = true, desc = "Show this file's Diff" })
 end
 
 -- Debug keymap
-if available("dap") then
-	keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", { silent = true, desc = "Continue" })
-	keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", { silent = true, desc = "Breakpoint" })
-	keymap("n", "<leader>do", "<cmd>lua require'dap'.step_over()<cr>", { silent = true, desc = "Step Over" })
-	keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", { silent = true, desc = "Step Into" })
-	keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.open()<cr>", { silent = true, desc = "inspect REPL" })
+local dap_ok, dap = pcall(require, "dap")
+if dap_ok then
+	keymap("n", "<leader>dc", function()
+		dap.continue()
+	end, { silent = true, desc = "Continue" })
+	keymap("n", "<leader>db", function()
+		dap.toggle_breakpoint()
+	end, { silent = true, desc = "Breakpoint" })
+	keymap("n", "<leader>do", function()
+		dap.step_over()
+	end, { silent = true, desc = "Step Over" })
+	keymap("n", "<leader>di", function()
+		dap.step_into()
+	end, { silent = true, desc = "Step Into" })
+	keymap("n", "<leader>dr", function()
+		dap.repl.open()
+	end, { silent = true, desc = "inspect REPL" })
 end
 
 -- ToggleTerm keymap
 if available("toggleterm") then
-	keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", { silent = true, desc = "Lazygit" })
-	keymap("n", "<leader>`t", "<cmd>lua _HTOP_TOGGLE()<cr>", { silent = true, desc = "Htop" })
-	keymap("n", "<leader>`p", "<cmd>lua _PYTHON_TOGGLE()<cr>", { silent = true, desc = "Python" })
-	keymap("n", "<leader>``", "<cmd>ToggleTerm direction=float<cr>", { silent = true, desc = "Float" })
-	keymap(
-		"n",
-		"<leader>`h",
-		"<cmd>ToggleTerm size=10 direction=horizontal<cr>",
-		{ silent = true, desc = "Horizontal" }
-	)
+	keymap("n", "<leader>gg", function()
+		_LAZYGIT_TOGGLE()
+	end, { silent = true, desc = "Lazygit" })
+	keymap("n", "<leader>`t", function()
+		_HTOP_TOGGLE()
+	end, { silent = true, desc = "Htop" })
+	keymap("n", "<leader>`p", function()
+		_PYTHON_TOGGLE()
+	end, { silent = true, desc = "Python" })
+	keymap("n", "<leader>``", function()
+		vim.cmd("ToggleTerm direction=float")
+	end, { silent = true, desc = "Float" })
+	keymap("n", "<leader>`h", function()
+		vim.cmd("ToggleTerm size=10 direction=horizontal")
+	end, { silent = true, desc = "Horizontal" })
 end
 -- Telescope
-if available("telescope") then
+local tel_status_ok, telescope = pcall(require, "telescope.builtin")
+if tel_status_ok then
 	keymap("n", "<leader>ff", function()
 		local fopts = { hidden = true } -- define here if you want to define something
-		local ok = pcall(require("telescope.builtin").git_files)
+		local ok = pcall(telescope.git_files)
 		if not ok then
-			require("telescope.builtin").find_files(fopts)
+			telescope.find_files(fopts)
 		end
 	end, { silent = true, desc = "Find Files" })
-	keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { silent = true, desc = "Show Buffers" })
-	keymap("n", "<leader>fc", "<cmd>Telescope colorscheme<cr>", { silent = true, desc = "Colorscheme" })
-	keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { silent = true, desc = "Find Help" })
-	keymap("n", "<leader>fM", "<cmd>Telescope man_pages<cr>", { silent = true, desc = "Man Pages" })
-	keymap("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { silent = true, desc = "Open Recent File" })
-	keymap("n", "<leader>fR", "<cmd>Telescope registers<cr>", { silent = true, desc = "Registers" })
-	keymap("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { silent = true, desc = "Keymaps" })
-	keymap("n", "<leader>fC", "<cmd>Telescope commands<cr>", { silent = true, desc = "Commands" })
-	keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { silent = true, desc = "Find word" })
-	keymap("n", "<leader>fp", "<cmd>Telescope projects<cr>", { silent = true, desc = "Projects" })
+	keymap("n", "<leader>fb", function()
+		telescope.buffers()
+	end, { silent = true, desc = "Show Buffers" })
+	keymap("n", "<leader>fc", function()
+		telescope.colorscheme()
+	end, { silent = true, desc = "Colorscheme" })
+	keymap("n", "<leader>fh", function()
+		telescope.help_tags()
+	end, { silent = true, desc = "Find Help" })
+	keymap("n", "<leader>fM", function()
+		telescope.man_pages()
+	end, { silent = true, desc = "Man Pages" })
+	keymap("n", "<leader>fr", function()
+		telescope.oldfiles()
+	end, { silent = true, desc = "Open Recent File" })
+	keymap("n", "<leader>fR", function()
+		telescope.registers()
+	end, { silent = true, desc = "Registers" })
+	keymap("n", "<leader>fk", function()
+		telescope.keymaps()
+	end, { silent = true, desc = "Keymaps" })
+	keymap("n", "<leader>fC", function()
+		telescope.commands()
+	end, { silent = true, desc = "Commands" })
+	keymap("n", "<leader>fg", function()
+		telescope.live_grep()
+	end, { silent = true, desc = "Find word" })
+	keymap("n", "<leader>fp", function()
+		telescope.projects()
+	end, { silent = true, desc = "Projects" })
 
-	keymap("n", "<leader>go", "<cmd>Telescope git_status<cr>", { silent = true, desc = "Open changed file" })
-	keymap("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", { silent = true, desc = "Checkout branch" })
-	keymap("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", { silent = true, desc = "Checkout commit" })
-	keymap(
-		"n",
-		"<leader>b",
-		"<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
-		{ silent = true, desc = "Buffers" }
-	)
+	keymap("n", "<leader>go", function()
+		telescope.git_status()
+	end, { silent = true, desc = "Open changed file" })
+	keymap("n", "<leader>gb", function()
+		telescope.git_branches()
+	end, { silent = true, desc = "Checkout branch" })
+	keymap("n", "<leader>gc", function()
+		telescope.git_commits()
+	end, { silent = true, desc = "Checkout commit" })
+	keymap("n", "<leader>b", function()
+		telescope.buffers(require("telescope.themes").get_dropdown({ previewer = false }))
+	end, { silent = true, desc = "Buffers" })
 end
 
 -- Packer Keympas
