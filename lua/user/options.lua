853bcf285b187cb12c2e219c88ccf0d95a7eea19
Arrow-x <Houssem.kun26@gmail.com>
2022-07-06 11:01:42 +0100

    disbale a bunch of vim builtin plugins

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,12 +1,10 @@
--- :help options
-O = vim.opt
-
+local O = vim.opt
 -- O.clipboard = "unnamedplus" --uses all the + register for clipboard
 O.cmdheight = 1 -- more space in the neovim command line for displaying messages
 O.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 O.conceallevel = 0 -- so that `` is visible in markdown files
 O.fileencoding = "utf-8" -- the encoding written to a file
--- o.mouse = "a"                             -- allow the mouse to be used in neovim
+-- O.mouse = "a"                             -- allow the mouse to be used in neovim
 O.showmode = false
 O.pumheight = 10 -- pop up menu height
 O.showtabline = 2 -- always show tabs
@@ -31,22 +29,39 @@ O.signcolumn = "yes" -- always show the sign column, otherwise it would shift th
 O.wrap = false -- display lines as one long line
 O.scrolloff = 8 -- is one of my fav
 O.sidescrolloff = 8
--- o.guifont = "monospace:h17"               -- the font used in graphical neovim applications
+-- O.guifont = "monospace:h17"               -- the font used in graphical neovim applications
 
 O.hlsearch = true -- highlight all matches on previous search pattern
 O.incsearch = true
 O.ignorecase = false -- ignore case in search patterns
-vim.cmd("highlight Normal guibg=none")
-
-O.smartindent = true -- make indenting smarter again
-O.expandtab = false -- convert tabs to spaces
-O.tabstop = 4 -- insert 4 spaces for a tab
 
 O.shortmess:append("c")
 -- O.whichwrap:append("<,>,[,],h,l")
 -- O.iskeyword:append("-")
 
+local G = vim.g
+G.do_filetype_lua = 1 -- use filetype.lua
+G.did_load_filetypes = 0 -- don't use filetype.vim
+G.highlighturl_enabled = true -- highlight URLs by default
+G.zipPlugin = false -- disable zip
+G.load_black = false -- disable black
+G.loaded_2html_plugin = true -- disable 2html
+G.loaded_getscript = true -- disable getscript
+G.loaded_getscriptPlugin = true -- disable getscript
+G.loaded_gzip = true -- disable gzip
+G.loaded_logipat = true -- disable logipat
+G.loaded_matchit = true -- disable matchit
+G.loaded_netrwFileHandlers = true -- disable netrw
+G.loaded_netrwPlugin = true -- disable netrw
+G.loaded_netrwSettngs = true -- disable netrw
+G.loaded_remote_plugins = true -- disable remote plugins
+G.loaded_tar = true -- disable tar
+G.loaded_tarPlugin = true -- disable tar
+G.loaded_zip = true -- disable zip
+G.loaded_zipPlugin = true -- disable zip
+G.loaded_vimball = true -- disable vimball
+G.loaded_vimballPlugin = true -- disable vimball
+
+vim.cmd("highlight Normal guibg=none")
 vim.cmd("set whichwrap+=<,>,[,],h,l")
 vim.cmd([[set iskeyword+=-]])
--- vim.cmd("let g:did_load_filetypes = 0")
--- vim.cmd("let g:do_filetype_lua = 1")
