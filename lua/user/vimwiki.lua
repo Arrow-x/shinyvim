7ae29b4101048d27082c757bda7a590d3ae29838
Arrow-x <Houssem.kun26@gmail.com>
2022-07-14 23:09:40 +0100

    vimwiki doesn't take over all markdown

--- lua/user/vimwiki.lua
+++ lua/user/vimwiki.lua
@@ -2,3 +2,4 @@ vim.cmd(
 	"let g:vimwiki_list = [{ 'path': '~/vimwiki', 'template_path': '~/vimwiki/templates/', 'template_default': 'default', 'syntax': 'markdown', 'ext': '.md', 'path_html': '~/vimwiki/site_html/', 'custom_wiki2html': 'vimwiki_markdown', 'template_ext': '.tpl', 'links_space_char': '_'}]"
 )
 vim.cmd("let g:vimwiki_markdown_link_ext = 1")
+vim.cmd("let g:vimwiki_global_ext = 0")
