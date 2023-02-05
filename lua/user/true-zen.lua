9af06b7ab184047017c90a8f43435d10cde553fe
Arrow-x <Houssem.kun26@gmail.com>
2023-02-05 16:42:33 +0100

    remove uselss code

--- lua/user/true-zen.lua
+++ lua/user/true-zen.lua
@@ -71,42 +71,6 @@ true_zen.setup({
 			open_callback = nil, -- run a function when opening Ataraxis mode
 			close_callback = nil, -- run a function when closing Ataraxis mode
 		},
-		minimalist = {
-			ignored_buf_types = { "nofile" }, -- save current options from any window except ones displaying these kinds of buffers
-			options = { -- options to be disabled when entering Minimalist mode
-				number = false,
-				relativenumber = false,
-				showtabline = 0,
-				signcolumn = "no",
-				statusline = "",
-				cmdheight = 1,
-				laststatus = 0,
-				showcmd = false,
-				showmode = false,
-				ruler = false,
-				numberwidth = 1,
-			},
-			open_callback = nil, -- run a function when opening Minimalist mode
-			close_callback = nil, -- run a function when closing Minimalist mode
-		},
-		narrow = {
-			--- change the style of the fold lines. Set it to:
-			--- `informative`: to get nice pre-baked folds
-			--- `invisible`: hide them
-			--- function() end: pass a custom func with your fold lines. See :h foldtext
-			folds_style = "informative",
-			run_ataraxis = true, -- display narrowed text in a Ataraxis session
-			open_callback = nil, -- run a function when opening Narrow mode
-			close_callback = nil, -- run a function when closing Narrow mode
-		},
-		focus = {
-			callbacks = { -- run functions when opening/closing Focus mode
-				open_pre = nil,
-				open_pos = nil,
-				close_pre = nil,
-				close_pos = nil,
-			},
-		},
 	},
 	integrations = {
 		tmux = true, -- hide tmux status bar in (minimalist, ataraxis)
