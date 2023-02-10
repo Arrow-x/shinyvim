2952d8488afaee9be7d95595669a1a2df8164fda
Arrow-x <Houssem.kun26@gmail.com>
2023-02-10 18:24:17 +0100

    treesitter: specific langs

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,18 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = {
+		"gdscript",
+		"python",
+		"bash",
+		"markdown",
+		"markdown_inline",
+		"rust",
+		"lua",
+		"c",
+		"cpp",
+		"c_sharp",
+	}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	-- autopairs = {
