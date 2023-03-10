e04828ebfd5828502a5d5bc009e367567d782206
Arrow-x <Houssem.kun26@gmail.com>
2023-03-10 22:17:47 +0100

    cmp: disable lsp when writing a comment rather then disable all of cmp

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -128,7 +128,13 @@ cmp.setup({
 		end,
 	},
 	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
+		{
+			name = "nvim_lsp",
+			enabled = function(ctx)
+				local context = require("cmp.config.context")
+				return not context.in_treesitter_capture("comment")
+			end,
+		},
 		{ name = "nvim_lua" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
@@ -137,16 +143,17 @@ cmp.setup({
 		{ name = "spell" },
 		-- { name = 'look'},
 	}),
-	enabled = function()
-		-- disable completion in comments
-		local context = require("cmp.config.context")
-		-- keep command mode completion enabled when cursor is in a comment
-		if vim.api.nvim_get_mode().mode == "c" then
-			return true
-		else
-			return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-		end
-	end,
+	-- enabled = function()
+	-- 	-- disable completion in comments
+	-- 	local context = require("cmp.config.context")
+	-- 	-- keep command mode completion enabled when cursor is in a comment
+	-- 	if vim.api.nvim_get_mode().mode == "c" then
+	-- 		return true
+	-- 	else
+	-- 		return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+	-- 	end
+	-- end,
+
 	-- formatting= {
 	--     format = lspkind.cmp_format({with_text = true,})
 	-- },
