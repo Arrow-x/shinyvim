36c5e3cc473de3ba715856a4681c771b60caf783
Arrow-x <Houssem.kun26@gmail.com>
2023-03-14 16:57:19 +0100

    add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,47 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	version = false, -- last release is way too old and doesn't work on Windows
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"vim",
+				"help",
+				"gdscript",
+				"python",
+				"bash",
+				"markdown",
+				"markdown_inline",
+				"rust",
+				"lua",
+				"c",
+				"cpp",
+				"c_sharp",
+			},              -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+			sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+			ignore_install = { "" }, -- List of parsers to ignore installing
+			-- autopairs = {
+			-- 	enable = true,
+			-- },
+			highlight = {
+				enable = true, -- false will disable the whole extension
+				disable = { "" }, -- list of language that will be disabled
+				additional_vim_regex_highlighting = true,
+			},
+			rainbow = {
+				enable = true,
+				-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+				extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+				max_file_lines = nil, -- Do not enable for files with more than n lines, int
+				-- colors = {}, -- table of hex strings
+				-- termcolors = {} -- table of colour name strings
+			},
+			indent = { enable = true, disable = { "yaml" } },
+			-- context_commentstring = {
+			-- 	enable = true,
+			-- 	enable_autocmd = false,
+			-- },
+		})
+	end,
+}
