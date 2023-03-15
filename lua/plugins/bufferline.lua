00d571b0ffc9eed5e9c5cf392071cd13d981d5af
Arrow-x <Houssem.kun26@gmail.com>
2023-03-15 17:37:10 +0100

    bufferline respects neotree

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,4 +1,17 @@
 return {
 	"akinsho/bufferline.nvim",
+	event = "VeryLazy",
+	opts = {
+		options = {
+			offsets = {
+				{
+					filetype = "neo-tree",
+					text = "Neo-tree",
+					highlight = "Directory",
+					text_align = "left",
+				},
+			},
+		},
+	},
 	config = true,
 }
