5cff78a405a661ebb6d86c7035726e9c9b19f229
Arrow-x <Houssem.kun26@gmail.com>
2023-03-15 20:13:16 +0100

    gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,50 +1,51 @@
 return {
+	-- BUG: This calls withit Toruble.nvim for some reason.
 	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPost", "BufNewFile" },
 	opts = {
 		signs = {
-			add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-			change = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-			delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-			topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-			changedelete = {
-				hl = "GitSignsChange",
-				text = "▎",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
+			add = { text = "▎" },
+			change = { text = "▎" },
+			delete = { text = "" },
+			topdelete = { text = "" },
+			changedelete = { text = "▎" },
+			untracked = { text = "▎" },
 		},
-	},
-	config = function()
-		local keymap = vim.keymap.set
-		local gitsigns = require("gitsigns")
+		on_attach = function(buffer)
+			local gitsigns = package.loaded.gitsigns
 
-		keymap("n", "<leader>gn", function()
-			gitsigns.next_hunk()
-		end, { desc = "Next Hunk" })
-		keymap("n", "<leader>gp", function()
-			gitsigns.prev_hunk()
-		end, { desc = "Prev Hunk" })
-		keymap("n", "<leader>gl", function()
-			gitsigns.blame_line()
-		end, { desc = "Blame" })
-		keymap("n", "<leader>gP", function()
-			gitsigns.preview_hunk()
-		end, { desc = "Preview Hunk" })
-		keymap("n", "<leader>gr", function()
-			gitsigns.reset_hunk()
-		end, { desc = "Reset Hunk" })
-		keymap("n", "<leader>gR", function()
-			gitsigns.reset_buffer()
-		end, { desc = "Reset Buffer" })
-		keymap("n", "<leader>gs", function()
-			gitsigns.stage_hunk()
-		end, { desc = "Stage Hunk" })
-		keymap("n", "<leader>gu", function()
-			gitsigns.undo_stage_hunk()
-		end, { desc = "Undo Stage Hunk" })
-		keymap("n", "<leader>gd", function()
-			vim.cmd("Gitsigns diffthis HEAD end")
-		end, { desc = "Show this file's Diff" })
-		gitsigns.setup()
-	end,
+			local function map(mode, l, r, desc)
+				vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+			end
+
+			map("n", "<leader>gn", function()
+				gitsigns.next_hunk()
+			end, { desc = "Next Hunk" })
+			map("n", "<leader>gp", function()
+				gitsigns.prev_hunk()
+			end, { desc = "Prev Hunk" })
+			map("n", "<leader>gl", function()
+				gitsigns.blame_line()
+			end, { desc = "Blame" })
+			map("n", "<leader>gP", function()
+				gitsigns.preview_hunk()
+			end, { desc = "Preview Hunk" })
+			map("n", "<leader>gr", function()
+				gitsigns.reset_hunk()
+			end, { desc = "Reset Hunk" })
+			map("n", "<leader>gR", function()
+				gitsigns.reset_buffer()
+			end, { desc = "Reset Buffer" })
+			map("n", "<leader>gs", function()
+				gitsigns.stage_hunk()
+			end, { desc = "Stage Hunk" })
+			map("n", "<leader>gu", function()
+				gitsigns.undo_stage_hunk()
+			end, { desc = "Undo Stage Hunk" })
+			map("n", "<leader>gd", function()
+				vim.cmd("Gitsigns diffthis HEAD end")
+			end, { desc = "Show Diff" })
+		end,
+	},
 }
+-- stylua: ignore start
