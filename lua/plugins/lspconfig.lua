0271f6ceb5821fa7d3a173120ed249f2828e7c5f
Arrow-x <Houssem.kun26@gmail.com>
2023-03-16 19:58:20 +0100

    chore:formating

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -55,9 +55,9 @@ return {
 			handlers.setup = function()
 				local signs = {
 					{ name = "DiagnosticSignError", text = "" },
-					{ name = "DiagnosticSignWarn",  text = "" },
-					{ name = "DiagnosticSignHint",  text = "" },
-					{ name = "DiagnosticSignInfo",  text = "" },
+					{ name = "DiagnosticSignWarn", text = "" },
+					{ name = "DiagnosticSignHint", text = "" },
+					{ name = "DiagnosticSignInfo", text = "" },
 				}
 
 				for _, sign in ipairs(signs) do
