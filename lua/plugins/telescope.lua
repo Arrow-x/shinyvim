e14e65df1bbe98106285391a3323aa75fac611a5
Arrow-x <Houssem.kun26@gmail.com>
2023-03-17 22:51:41 +0100

    telescope remove fzf sorter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-		cond = function()
-			return vim.fn.executable("make") == 1
-		end,
-	},
 	config = function()
-		pcall(require("telescope").load_extension, "fzf")
 	end,
 	keys = {
 		{
