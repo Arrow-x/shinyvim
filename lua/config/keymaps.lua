8328869bfef179355aee60db06334554f35e3e0b
Arrow-x <Houssem.kun26@gmail.com>
2023-03-20 02:10:31 +0100

    removing some uselsss stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -82,8 +82,6 @@ keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
--- keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
--- keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<C-j>", ":move '>+1<CR>gv=gv", opts)
 keymap("x", "<C-k>", ":move '<-2<CR>gv=gv", opts)
 
