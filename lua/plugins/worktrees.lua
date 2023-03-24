1c2406ac36dae280124709d886312255e3c1a368
Arrow-x <Houssem.kun26@gmail.com>
2023-03-24 03:47:10 +0100

    git worktees workflow support, so far just the switching

---
+++ lua/plugins/worktrees.lua
@@ -0,0 +1,19 @@
+return {
+	"ThePrimeagen/git-worktree.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+	},
+	config = {
+		function()
+			require("telescope").load_extension("git_worktree")
+		end,
+	},
+	keys = {
+		{
+			"<leader>gw",
+			function()
+				require("telescope").extensions.git_worktree.git_worktrees()
+			end,
+		},
+	},
+}
