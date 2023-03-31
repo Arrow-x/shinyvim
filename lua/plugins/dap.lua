0b1e8fc7c77561d8afcf42d9213160fa12950c97
Arrow-x <Houssem.kun26@gmail.com>
2023-03-31 23:36:40 +0100

    dap: make dap the main plugin and the rest as depend

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,43 +1,45 @@
 return {
-	"rcarriga/nvim-dap-ui",
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
-		dapui.setup({
-			layouts = {
-				{
-					elements = {
-						"scopes",
-						"breakpoints",
-						"stacks",
-						"watches",
-					},
-					size = 40,
-					position = "left",
-				},
-				{
-					elements = {
-						"repl",
-						"console",
-					},
-					size = 10,
-					position = "bottom",
-				},
-			},
-		})
-
-		dap.listeners.after.event_initialized["dapui_config"] = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated["dapui_config"] = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited["dapui_config"] = function()
-			dapui.close()
-		end
-	end,
+	"mfussenegger/nvim-dap",
 	dependencies = {
-		"mfussenegger/nvim-dap",
+		{
+			"rcarriga/nvim-dap-ui",
+			config = function()
+				local dap = require("dap")
+				local dapui = require("dapui")
+				dapui.setup({
+					layouts = {
+						{
+							elements = {
+								"scopes",
+								"breakpoints",
+								"stacks",
+								"watches",
+							},
+							size = 40,
+							position = "left",
+						},
+						{
+							elements = {
+								"repl",
+								"console",
+							},
+							size = 10,
+							position = "bottom",
+						},
+					},
+				})
+
+				dap.listeners.after.event_initialized["dapui_config"] = function()
+					dapui.open()
+				end
+				dap.listeners.before.event_terminated["dapui_config"] = function()
+					dapui.close()
+				end
+				dap.listeners.before.event_exited["dapui_config"] = function()
+					dapui.close()
+				end
+			end,
+		},
 		{
 			"theHamsta/nvim-dap-virtual-text",
 			opts = {
