258bb9e82c2dc8ba1afe5009676b4bd42942f88a
Arrow-x <Houssem.kun26@gmail.com>
2023-04-22 00:31:54 +0100

    finaly got fzf sorter working with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,10 +6,27 @@ return {
 			defaults = {
 				file_ignore_patterns = { "^.git" },
 			},
+			extensions = {
+				fzf = {
+					fuzzy = true, -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					-- the default case_mode is "smart_case"
+				},
+			},
 		})
+		require("telescope").load_extension("fzf")
 	end,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
 	},
 	keys = {
 		{
