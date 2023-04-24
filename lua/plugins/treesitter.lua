9593b3cfbc7f7b5891b8240f9b34d2bfe7479784
Arrow-x <Houssem.kun26@gmail.com>
2023-04-25 00:10:29 +0100

    treesitter doesn't use vim regex highlighs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	version = false, -- last release is way too old and doesn't work on Windows
 	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
 	dependencies = {
 		"p00f/nvim-ts-rainbow",
 	},
@@ -21,29 +21,27 @@ return {
 				"cpp",
 				"c_sharp",
 			}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-			sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+			sync_install = true, -- install languages synchronously (only applied to `ensure_installed`)
 			ignore_install = { "" }, -- List of parsers to ignore installing
-			-- autopairs = {
-			-- 	enable = true,
-			-- },
 			highlight = {
 				enable = true, -- false will disable the whole extension
-				disable = { "" }, -- list of language that will be disabled
-				additional_vim_regex_highlighting = true,
+				disable = function(_, bufnr)
+					return vim.api.nvim_buf_line_count(bufnr) > 10000
+				end,
+				additional_vim_regex_highlighting = false,
 			},
 			rainbow = {
 				enable = true,
 				-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
 				extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-				max_file_lines = nil, -- Do not enable for files with more than n lines, int
+				max_file_lines = 10000, -- Do not enable for files with more than n lines, int
 				-- colors = {}, -- table of hex strings
 				-- termcolors = {} -- table of colour name strings
 			},
-			indent = { enable = true, disable = { "yaml" } },
-			-- context_commentstring = {
-			-- 	enable = true,
-			-- 	enable_autocmd = false,
-			-- },
+			incremental_selection = { enable = true },
+			indent = { enable = true },
+			autotag = { enable = true },
+			context_commentstring = { enable = true, enable_autocmd = false },
 		})
 	end,
 }
