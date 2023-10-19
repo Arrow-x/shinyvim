33918dce466c1647f0fbb7d4d3f2247aec8c60cb
Arrow-x <Houssem.kun26@gmail.com>
2023-10-19 11:27:35 +0100

    update some configs

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,1 +1,5 @@
-return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = { scope = { enabled = true, show_start = false, show_end = false } },
+}
