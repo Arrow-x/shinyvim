c76973a6be721c0246b198753a4988222cda1cbb
Arrow-x <Houssem.kun26@gmail.com>
2023-11-01 13:40:51 +0100

    replace bufferline and bbye with barbar

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,16 @@
+return {
+	"romgrk/barbar.nvim",
+	-- event = { "BufReadPost", "BufNewFile" },
+	dependencies = {
+		"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	config = function()
+		local map = vim.keymap.set
+		-- Move buffer
+		map("n", "<C-A-h>", "<Cmd>BufferMovePrevious<CR>")
+		map("n", "<C-A-l>", "<Cmd>BufferMoveNext<CR>")
+		-- Close buffer
+		map("n", "<leader>q", "<Cmd>BufferClose<CR>")
+	end,
+}
--- lua/plugins/bbye.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"moll/vim-bbye",
-	keys = {
-		{ "<leader>Q", ":%bd|e#<CR>", desc = "Close All Buffers" },
-		{ "<leader>q", "<cmd>Bdelete!<CR>", desc = "Close Buffer" },
-	},
-}
