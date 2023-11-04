ecbe54ab9a29779282fa893174a454bea69699dd
Arrow-x <Houssem.kun26@gmail.com>
2023-11-04 13:51:04 +0100

    refresh neotree afre lazygit exits

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -173,3 +173,20 @@ autocmd("TermOpen", {
 		vim.opt_local.signcolumn = "no"
 	end,
 })
+
+autocmd("TermClose", {
+	pattern = "*lazygit*",
+	desc = "Refresh Neo-Tree when closing lazygit",
+	group = gr.ui,
+	callback = function()
+		local manager_avail, manager = pcall(require, "neo-tree.sources.manager")
+		if manager_avail then
+			for _, source in ipairs({ "filesystem", "git_status", "document_symbols" }) do
+				local module = "neo-tree.sources." .. source
+				if package.loaded[module] then
+					manager.refresh(require(module).name)
+				end
+			end
+		end
+	end,
+})
