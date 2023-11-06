fb6949686acc60367c9a71d990eff3eedce1dc42
Arrow-x <Houssem.kun26@gmail.com>
2023-11-06 23:12:28 +0100

    add a function to toggle pairs

--- lua/plugins/mini_pairs.lua
+++ lua/plugins/mini_pairs.lua
@@ -13,5 +13,18 @@ return {
 		{ ")", mode = "i" },
 		{ "[", mode = "i" },
 		{ "]", mode = "i" },
+		{
+			"<leader>mp",
+			function()
+				local Util = require("lazy.core.util")
+				vim.g.minipairs_disable = not vim.g.minipairs_disable
+				if vim.g.minipairs_disable then
+					Util.warn("Disabled auto pairs", { title = "Option" })
+				else
+					Util.info("Enabled auto pairs", { title = "Option" })
+				end
+			end,
+			desc = "Toggle auto pairs",
+		},
 	},
 }
