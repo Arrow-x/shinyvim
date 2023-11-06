abf07f0030cc836c6a968b410dd490a4a2873837
Arrow-x <Houssem.kun26@gmail.com>
2023-11-06 13:23:43 +0100

    Obsidian use xdg-open for now

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -87,7 +87,7 @@ return {
 		follow_url_func = function(url)
 			-- Open the URL in the default web browser.
 			-- vim.fn.jobstart({ "open", url }) -- Mac OS
-			vim.fn.jobstart({ "link-handeler", url }) -- linux
+			vim.fn.jobstart({ "xdg-open", url }) -- linux
 		end,
 		-- Optional, set to true if you use the Obsidian Advanced URI plugin.
 		-- https://github.com/Vinzent03/obsidian-advanced-uri
