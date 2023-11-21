5e3092645a49b81cc9700de66a1e4111fcef82bd
Arrow-x <Houssem.kun26@gmail.com>
2023-11-21 20:01:38 +0100

    wider column bar and lsp and gitsigns can co-exist

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -72,3 +72,52 @@ function shinyvim.get_root()
 	---@cast root string
 	return root
 end
+
+function shinyvim.show_only_one_sign_in_sign_column()
+	---custom namespace
+	local ns = vim.api.nvim_create_namespace("severe-diagnostics")
+
+	---reference to the original handler
+	local orig_signs_handler = vim.diagnostic.handlers.signs
+
+	local function filter_diagnostics(diagnostics)
+		if not diagnostics then
+			return {}
+		end
+
+		-- find the "worst" diagnostic per line
+		local most_severe = {}
+		for _, cur in pairs(diagnostics) do
+			local max = most_severe[cur.lnum]
+
+			-- higher severity has lower value (`:h diagnostic-severity`)
+			if not max or cur.severity < max.severity then
+				most_severe[cur.lnum] = cur
+			end
+		end
+
+		-- return list of diagnostics
+		return vim.tbl_values(most_severe)
+	end
+	---Overriden diagnostics signs helper to only show the single most relevant sign
+	---@see `:h diagnostic-handlers`
+	vim.diagnostic.handlers.signs = {
+		show = function(_, bufnr, _, opts)
+			-- get all diagnostics from the whole buffer rather
+			-- than just the diagnostics passed to the handler
+			local diagnostics = vim.diagnostic.get(bufnr)
+
+			local filtered_diagnostics = filter_diagnostics(diagnostics)
+
+			-- pass the filtered diagnostics (with the
+			-- custom namespace) to the original handler
+			orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
+		end,
+
+		hide = function(_, bufnr)
+			orig_signs_handler.hide(ns, bufnr)
+		end,
+	}
+end
+
+shinyvim.show_only_one_sign_in_sign_column()
