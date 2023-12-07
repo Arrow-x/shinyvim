0c3c028ee1db9e724d823d6776bf4d9349856929
Arrow-x <Houssem.kun26@gmail.com>
2023-12-07 02:39:01 +0100

    use less ominisharp features, it lags less
    ivestigate later which option is making nvim lag

--- lua/config/lsp/settings/omnisharp.lua
+++ lua/config/lsp/settings/omnisharp.lua
@@ -10,7 +10,7 @@ return {
 	},
 	-- Enables support for reading code style, naming convention and analyzer
 	-- settings from .editorconfig.
-	enable_editorconfig_support = true,
+	enable_editorconfig_support = false,
 
 	-- If true, MSBuild project system will only load projects for files that
 	-- were opened in the editor. This setting is useful for big C# codebases
@@ -18,14 +18,14 @@ return {
 	-- for projects that are relevant to code that is being edited. With this
 	-- setting enabled OmniSharp may load fewer projects and may thus display
 	-- incomplete reference lists for symbols.
-	enable_ms_build_load_projects_on_demand = false,
+	enable_ms_build_load_projects_on_demand = true,
 
 	-- Enables support for roslyn analyzers, code fixes and rulesets.
 	enable_roslyn_analyzers = false,
 
 	-- Specifies whether 'using' directives should be grouped and sorted during
 	-- document formatting.
-	organize_imports_on_format = false,
+	organize_imports_on_format = true,
 
 	-- Enables support for showing unimported types and unimported extension
 	-- methods in completion lists. When committed, the appropriate using
@@ -33,7 +33,7 @@ return {
 	-- have a negative impact on initial completion responsiveness,
 	-- particularly for the first few completion sessions after opening a
 	-- solution.
-	enable_import_completion = false,
+	enable_import_completion = true,
 
 	-- Specifies whether to include preview versions of the .NET SDK when
 	-- determining which version to use for project loading.
@@ -41,5 +41,5 @@ return {
 
 	-- Only run analyzers against open files when 'enableRoslynAnalyzers' is
 	-- true
-	analyze_open_documents_only = false,
+	analyze_open_documents_only = true,
 }
