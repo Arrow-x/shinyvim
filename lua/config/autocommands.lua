6d6425cb64ca154235d800eef95976064ed918f7
Arrow-x <Houssem.kun26@gmail.com>
2023-12-15 15:13:41 +0100

    not needed

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -191,11 +191,11 @@ autocmd("TermClose", {
 	end,
 })
 
-autocmd("WinLeave", {
-	group = gr.general_settings,
-	callback = function()
-		if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
-			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
-		end
-	end,
-})
+-- autocmd("WinLeave", {
+-- 	group = gr.general_settings,
+-- 	callback = function()
+-- 		if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+-- 			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+-- 		end
+-- 	end,
+-- })
