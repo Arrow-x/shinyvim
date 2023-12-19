8d865195913e1f199df94039dc30567df2cb091e
Arrow-x <Houssem.kun26@gmail.com>
2023-12-19 23:44:29 +0100

    move treesitter context to be lazy loaded, and remove rainbow due slowness

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,42 +4,6 @@ return {
 	build = ":TSUpdate",
 	event = { "BufReadPost", "BufNewFile" },
 	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-	dependencies = {
-		{ "p00f/nvim-ts-rainbow" },
-		{
-			"nvim-treesitter/nvim-treesitter-context",
-			opts = {
-				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-				line_numbers = true,
-				multiline_threshold = 20, -- Maximum number of lines to show for a single context
-				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-				-- Separator between context and content. Should be a single character string, like '-'.
-				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-				separator = nil,
-				zindex = 20, -- The Z-index of the context window
-				on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-			},
-			keys = {
-				{
-					"<leader>mt",
-					function()
-						local tsc = require("treesitter-context")
-						tsc.toggle()
-						shinyvim.ts_context_toggle = not shinyvim.ts_context_toggle
-						if shinyvim.ts_context_toggle == true then
-							vim.notify("Treesitter Context is Turned On")
-						else
-							vim.notify("Treesitter Context is Turned Off")
-						end
-					end,
-					desc = "Toggle Treesitter Context",
-				},
-			},
-		},
-	},
 	config = vim.defer_fn(function()
 		local opts = {
 			ensure_installed = {
@@ -67,12 +31,7 @@ return {
 				additional_vim_regex_highlighting = { "markdown" },
 			},
 			rainbow = {
-				enable = true,
-				-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-				extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-				max_file_lines = 10000, -- Do not enable for files with more than n lines, int
-				-- colors = {}, -- table of hex strings
-				-- termcolors = {} -- table of colour name strings
+				enable = false,
 			},
 			incremental_selection = { enable = true },
 			indent = { enable = true, disable = { "gdscript" } },
