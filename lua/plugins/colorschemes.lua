16359a9e79807a1aecbfa9edbf69cf1c0692a198
Arrow-x <Houssem.kun26@gmail.com>
2023-12-22 17:22:55 +0100

    use night mode

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -6,7 +6,7 @@ return {
 		config = function()
 			-- load the colorscheme here
 			require("tokyonight").setup({
-				style = "storm",
+				style = "night",
 				light_style = "day", -- The theme is used when the background is set to light
 				transparent = true, -- Enable this to disable setting the background color
 				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
