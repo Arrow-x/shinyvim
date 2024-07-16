d37fee4ad1f099feff70efc0ba54f4e8cbb8a271
Arrow-x <Houssem.kun26@gmail.com>
2024-07-16 18:45:41 +0100

    new version of which-key

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,37 +1,40 @@
 return {
 	"folke/which-key.nvim",
+	dependencies = {
+		"https://github.com/echasnovski/mini.icons",
+	},
 	config = function()
 		local pluging_keymaps = {
-			{ plugin = "nvim-dap", prefix = "b", desc = "Debuging" },
-			{ plugin = "telescope.nvim", prefix = "f", desc = "Search" },
-			{ plugin = "gitsigns.nvim", prefix = "g", desc = "Git integration" },
-			{ plugin = "toggleterm.nvim", prefix = "`", desc = "A Terminal in your Terminal" },
-			{ plugin = "nvim-lspconfig", prefix = "l", desc = "LSP" },
-			{ plugin = "harpoon", prefix = "s", desc = "Harpoon" },
-			{ plugin = "true-zen.nvim", prefix = "z", desc = "TrueZen modes" },
-			{ plugin = "git-worktree.nvim", prefix = "gw", desc = "Worktrees" },
-			{ plugin = "trouble.nvim", prefix = "x", desc = "Trouble" },
-			{ plugin = "obsidian.nvim", prefix = "o", desc = "Obsidian" },
-			{ plugin = "aerial.nvim", prefix = "a", desc = "Aerial Toggle" },
-			{ plugin = "undotree", prefix = "u", desc = "UndoTree" },
-			{ plugin = "lazy.nvim", prefix = "z", desc = "Toggle Lazy" },
-			{ plugin = "hop.nvim", prefix = "j", desc = "Toggle Hop" },
-			{ plugin = "hover.nvim", prefix = "k", desc = "Toggle Hover" },
+			{ plugin = "nvim-dap", prefix = "<leader>b", group = "Debuging" },
+			{ plugin = "telescope.nvim", prefix = "<leader>f", group = "Search" },
+			{ plugin = "gitsigns.nvim", prefix = "<leader>g", group = "Git" },
+			{ plugin = "toggleterm.nvim", prefix = "<leader>`", group = "Terminal" },
+			{ plugin = "nvim-lspconfig", prefix = "<leader>l", group = "LSP" },
+			{ plugin = "harpoon", prefix = "<leader>s", group = "Harpoon" },
+			{ plugin = "true-zen.nvim", prefix = "<leader>z", group = "TrueZen" },
+			{ plugin = "git-worktree.nvim", prefix = "gw", group = "Worktrees" },
+			{ plugin = "trouble.nvim", prefix = "<leader>x", group = "Trouble" },
+			{ plugin = "obsidian.nvim", prefix = "<leader>o", group = "Obsidian" },
+			{ plugin = "undotree", prefix = "<leader>u", group = "UndoTree" },
+			{ plugin = "lazy.nvim", prefix = "<leader>z", group = "Lazy" },
+			{ plugin = "hop.nvim", prefix = "<leader>j", group = "Hop" },
+			{ plugin = "hover.nvim", prefix = "<leader>k", group = "Hover" },
 		}
 
 		local mappings = {
-			p = { name = "Paste from primary system clipboard" },
-			P = { name = "Paste from scondary system clipboard" },
-			y = { name = "Copy to System clipboard" },
-			m = { name = "Misc" },
+			{ "<leader>P", desc = "Paste from scondary system clipboard", nowait = true, remap = false, hidden = true },
+			{ "<leader>p", desc = "Paste from primary system clipboard", nowait = true, remap = false, hidden = true },
+			{ "<leader>y", desc = "Copy to System clipboard", nowait = true, remap = false, hidden = true },
+			{ "<leader>Y", hidden = true },
+			{ "<leader>d", hidden = true },
+			{ "<leader>D", hidden = true },
+			{ "<leader>m", group = "Misc", nowait = true, remap = false },
 		}
 
 		local opts = {
 			mode = "n",
-			prefix = "<leader>",
-			buffer = nil,
 			silent = true,
-			noremap = true,
+			remap = false,
 			nowait = true,
 		}
 
@@ -42,24 +45,205 @@ return {
 		local function check()
 			for _, p in pairs(pluging_keymaps) do
 				if has(p.plugin) then
-					if not mappings[p.prefix] then
-						mappings[p.prefix] = { name = p.desc }
-					end
+					table.insert(mappings, {
+						p.prefix,
+						group = p.group,
+						mode = opts.mode,
+						nowait = opts.nowait,
+						remap = opts.remap,
+						silent = opts.silent,
+					})
 				end
 			end
 		end
 		check()
-		require("which-key").register(mappings, opts)
-		require("which-key").setup({
+		local wk = require("which-key")
+		wk.setup({
+			---@type false | "classic" | "modern" | "helix"
+			preset = "modern",
+			-- Delay before showing the popup. Can be a number or a function that returns a number.
+			---@type number | fun(ctx: { keys: string, mode: string, plugin?: string }):number
+			delay = function(ctx)
+				return ctx.plugin and 0 or 200
+			end,
+			---@param mapping wk.Mapping
+			filter = function(mapping)
+				-- example to exclude mappings without a description
+				-- return mapping.desc and mapping.desc ~= ""
+				return true
+			end,
+			--- You can add any mappings here, or use `require('which-key').add()` later
+			---@type wk.Spec
+			spec = {},
+			-- show a warning when issues were detected with your mappings
+			notify = true,
+			-- Enable/disable WhichKey for certain mapping modes
+			modes = {
+				n = true, -- Normal mode
+				i = true, -- Insert mode
+				x = true, -- Visual mode
+				s = true, -- Select mode
+				o = true, -- Operator pending mode
+				t = true, -- Terminal mode
+				c = true, -- Command mode
+				-- Start hidden and wait for a key to be pressed before showing the popup
+				-- Only used by enabled xo mapping modes.
+				-- Set to false to show the popup immediately (after the delay)
+				defer = {
+					["<C-V>"] = true,
+					V = true,
+				},
+			},
 			plugins = {
-				spelling = { enabled = true },
-				presets = { operators = false },
+				marks = true, -- shows a list of your marks on ' and `
+				registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+				-- the presets plugin, adds help for a bunch of default keybindings in Neovim
+				-- No actual key bindings are created
+				spelling = {
+					enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+					suggestions = 20, -- how many suggestions should be shown in the list?
+				},
+				presets = {
+					operators = true, -- adds help for operators like d, y, ...
+					motions = true, -- adds help for motions
+					text_objects = true, -- help for text objects triggered after entering an operator
+					windows = true, -- default bindings on <c-w>
+					nav = true, -- misc bindings to work with windows
+					z = true, -- bindings for folds, spelling and others prefixed with z
+					g = true, -- bindings for prefixed with g
+				},
 			},
-			window = {
-				border = "double",
-				padding = { 2, 2, 2, 2 },
+			---@type wk.Win.opts
+			win = {
+				-- don't allow the popup to overlap with the cursor
+				no_overlap = true,
+				-- width = 1,
+				-- height = { min = 4, max = 25 },
+				-- col = 0,
+				-- row = math.huge,
+				-- border = "none",
+				padding = { 1, 2 }, -- extra window padding [top/bottom, right/left]
+				title = true,
+				title_pos = "center",
+				zindex = 1000,
+				-- Additional vim.wo and vim.bo options
+				bo = {},
+				wo = {
+					-- winblend = 10, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+				},
 			},
+			layout = {
+				width = { min = 20 }, -- min and max width of the columns
+				spacing = 3, -- spacing between columns
+				align = "left", -- align columns left, center or right
+			},
+			keys = {
+				scroll_down = "<c-d>", -- binding to scroll down inside the popup
+				scroll_up = "<c-u>", -- binding to scroll up inside the popup
+			},
+			---@type (string|wk.Sorter)[]
+			--- Mappings are sorted using configured sorters and natural sort of the keys
+			--- Available sorters:
+			--- * local: buffer-local mappings first
+			--- * order: order of the items (Used by plugins like marks / registers)
+			--- * group: groups last
+			--- * alphanum: alpha-numerical first
+			--- * mod: special modifier keys last
+			--- * manual: the order the mappings were added
+			--- * case: lower-case first
+			sort = { "local", "order", "group", "alphanum", "mod" },
+			---@type number|fun(node: wk.Node):boolean?
+			expand = 0, -- expand groups when <= n mappings
+			-- expand = function(node)
+			--   return not node.desc -- expand all nodes without a description
+			-- end,
+			-- Functions/Lua Patterns for formatting the labels
+			---@type table<string, ({[1]:string, [2]:string}|fun(str:string):string)[]>
+			replace = {
+				key = {
+					function(key)
+						return require("which-key.view").format(key)
+					end,
+					-- { "<Space>", "SPC" },
+				},
+				desc = {
+					{ "<Plug>%(?(.*)%)?", "%1" },
+					{ "^%+", "" },
+					{ "<[cC]md>", "" },
+					{ "<[cC][rR]>", "" },
+					{ "<[sS]ilent>", "" },
+					{ "^lua%s+", "" },
+					{ "^call%s+", "" },
+					{ "^:%s*", "" },
+				},
+			},
+			icons = {
+				breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+				separator = "➜", -- symbol used between a key and it's label
+				group = "+", -- symbol prepended to a group
+				ellipsis = "…",
+				-- set to false to disable all mapping icons,
+				-- both those explicitely added in a mapping
+				-- and those from rules
+				mappings = true,
+				--- See `lua/which-key/icons.lua` for more details
+				--- Set to `false` to disable keymap icons from rules
+				---@type wk.IconRule[]|false
+				rules = {},
+				-- use the highlights from mini.icons
+				-- When `false`, it will use `WhichKeyIcon` instead
+				colors = true,
+				-- used by key format
+				keys = {
+					Up = " ",
+					Down = " ",
+					Left = " ",
+					Right = " ",
+					C = "󰘴 ",
+					M = "󰘵 ",
+					D = "󰘳 ",
+					S = "󰘶 ",
+					CR = "󰌑 ",
+					Esc = "󱊷 ",
+					ScrollWheelDown = "󱕐 ",
+					ScrollWheelUp = "󱕑 ",
+					NL = "󰌑 ",
+					BS = "󰁮",
+					Space = "󱁐 ",
+					Tab = "󰌒 ",
+					F1 = "󱊫",
+					F2 = "󱊬",
+					F3 = "󱊭",
+					F4 = "󱊮",
+					F5 = "󱊯",
+					F6 = "󱊰",
+					F7 = "󱊱",
+					F8 = "󱊲",
+					F9 = "󱊳",
+					F10 = "󱊴",
+					F11 = "󱊵",
+					F12 = "󱊶",
+				},
+			},
+			show_help = true, -- show a help message in the command line for using WhichKey
+			show_keys = true, -- show the currently pressed key and its label as a message in the command line
+			-- Which-key automatically sets up triggers for your mappings.
+			-- But you can disable this and setup the triggers yourself.
+			-- Be aware, that triggers are not needed for visual and operator pending mode.
+			triggers = true, -- automatically setup triggers
+			disable = {
+				-- disable WhichKey for certain buf types and file types.
+				ft = {},
+				bt = {},
+				-- disable a trigger for a certain context by returning true
+				---@type fun(ctx: { keys: string, mode: string, plugin?: string }):boolean?
+				trigger = function(ctx)
+					return false
+				end,
+			},
+			debug = false, -- enable wk.log in the current directory
 		})
+		wk.add(mappings)
 	end,
 	keys = {
 		"<leader>",
