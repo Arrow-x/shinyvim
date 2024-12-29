60c69d4534f7df7143547c544316575a4c40357d
Arrow-x <Houssem.kun26@gmail.com>
2024-12-29 13:30:28 +0100

    useless options

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -5,14 +5,14 @@ return {
 		require("notify").setup({
 			background_colour = "#000000",
 			timeout = 3000,
-			max_height = function()
-				return math.floor(vim.o.lines * 0.75)
-			end,
-			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
-			end,
-			render = "wrapped-compact",
-			stages = "slide",
+			-- max_height = function()
+			-- 	return math.floor(vim.o.lines * 0.75)
+			-- end,
+			-- max_width = function()
+			-- 	return math.floor(vim.o.columns * 0.75)
+			-- end,
+			-- render = "wrapped-compact",
+			-- stages = "slide",
 		})
 		vim.notify = require("notify")
 		vim.keymap.set("n", "<leader>mh", function()
