e8c3a8c98c3fd7090d61d8e0228767a1d90e9c67
Arrow-x <Houssem.kun26@gmail.com>
2025-01-07 16:01:03 +0100

    dispatch some keybinds

--- lua/plugins/dispatch.lua
+++ lua/plugins/dispatch.lua
@@ -1,5 +1,21 @@
 return {
-	ft = { "gdscript" },
-	cmd = { "Make", "Dispatch" },
 	"tpope/vim-dispatch",
+	lazy = false,
+	keys = {
+		{
+			"<leader>dd",
+			"<cmd>Dispatch<CR>",
+			desc = "Dispatch",
+		},
+		{
+			"<leader>dm",
+			"<cmd>Make<CR>",
+			desc = "run :make async",
+		},
+		{
+			"<leader>do",
+			"<cmd>Copen<CR>",
+			desc = "Open the pane where Dispatch made it stuff",
+		},
+	},
 }
