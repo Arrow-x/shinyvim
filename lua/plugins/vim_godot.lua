e3b94eec184520e1313f2ed8224a68a9e71d6d44
Arrow-x <Houssem.kun26@gmail.com>
2025-01-07 16:00:23 +0100

    vim_godot: has some keybinds

--- lua/plugins/vim_godot.lua
+++ lua/plugins/vim_godot.lua
@@ -2,6 +2,29 @@ return {
 	"habamax/vim-godot",
 	ft = { "gdscript" },
 	init = function()
-		vim.cmd("let g:godot_executable = '~/.local/bin/godot'")
+		vim.g.godot_executable = "~/.local/bin/godot"
 	end,
+	keys = {
+		{
+			"<leader>rr",
+			function()
+				vim.cmd("GodotRun")
+			end,
+			desc = "Run the initial Godot Scene",
+		},
+		{
+			"<leader>rc",
+			function()
+				vim.cmd("GodotRunCurrent")
+			end,
+			desc = "Run the Current Godot Scene",
+		},
+		{
+			"<leader>rl",
+			function()
+				vim.cmd("GodotRunLast")
+			end,
+			desc = "Run the Last Godot Scene",
+		},
+	},
 }
