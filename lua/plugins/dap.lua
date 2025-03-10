a327feb72bb59c137ae72cc9a2c0c00906d0dd80
Arrow-x <Houssem.kun26@gmail.com>
2025-03-10 22:10:04 +0100

    add c++ debugging

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -191,6 +191,35 @@ return {
 				launch_scene = true,
 			},
 		}
+		dap.adapters.cppdbg = {
+			id = "cppdbg",
+			type = "executable",
+			command = vim.fs.normalize(vim.fn.stdpath("data") .. "/mason/bin/OpenDebugAD7"),
+		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch file",
+				type = "cppdbg",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtEntry = true,
+			},
+			{
+				name = "Attach to gdbserver :1234",
+				type = "cppdbg",
+				request = "launch",
+				MIMode = "gdb",
+				miDebuggerServerAddress = "localhost:1234",
+				miDebuggerPath = "/usr/bin/gdb",
+				cwd = "${workspaceFolder}",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+			},
+		}
 	end,
 	keys = {
 		{
