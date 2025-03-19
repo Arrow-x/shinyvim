86be4038d484651cb66af4286be6e0e8d62ce5b6
Arrow-x <Houssem.kun26@gmail.com>
2025-03-19 17:27:00 +0100

    use lf as the main file manager

--- lua/plugins/fm.lua
+++ lua/plugins/fm.lua
@@ -1,4 +1,18 @@
 return {
 	"is0n/fm-nvim",
 	cmd = { "Lf", "Fzf" },
+	keys = {
+		{
+			"<leader>e",
+			function()
+				local current_file = vim.api.nvim_buf_get_name(0)
+				if current_file == nil or current_file == "" or string.match(current_file, "^" .. "oil") then
+					vim.cmd("Lf")
+				else
+					vim.cmd("Lf %")
+				end
+			end,
+			desc = "Open Lf File Manager",
+		},
+	},
 }
