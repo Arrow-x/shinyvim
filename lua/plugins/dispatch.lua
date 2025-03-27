042daa196a425cda0584250a987562f034101f2b
Arrow-x <Houssem.kun26@gmail.com>
2025-03-27 23:57:09 +0100

    dispatch keys are useless, lazy load on cmd

--- lua/plugins/dispatch.lua
+++ lua/plugins/dispatch.lua
@@ -1,21 +1,4 @@
 return {
 	"tpope/vim-dispatch",
-	lazy = false,
-	keys = {
-		{
-			"<leader>dd",
-			"<cmd>Dispatch<CR>",
-			desc = "Dispatch",
-		},
-		{
-			"<leader>dm",
-			"<cmd>Make<CR>",
-			desc = "run :make async",
-		},
-		{
-			"<leader>do",
-			"<cmd>Copen<CR>",
-			desc = "Open the pane where Dispatch made it stuff",
-		},
-	},
+	cmd = {"Dispatch", "FocusDispatch", "Make"},
 }
