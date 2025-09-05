0b34abf2ed61b549011144df71fcb8e62fe1658e
Arrow-x <Houssem.kun26@gmail.com>
2025-09-05 16:45:09 +0100

    a central place for language specific, project specific options

---
+++ after/ftplugin/cpp.lua
@@ -0,0 +1,17 @@
+-- ~/.config/nvim/after/ftplugin/cpp.lua
+local project_root = vim.fn.getcwd()
+
+vim.bo.errorformat = "%f:%l:%c: %m"
+
+if vim.fn.filereadable(project_root .. "/SConstruct") == 1 then
+	-- Scons project
+	vim.bo.makeprg = "scons target=template_debug debug_symbols=yes"
+elseif vim.fn.filereadable(project_root .. "/CMakeLists.txt") == 1 then
+	-- CMake project
+	vim.bo.makeprg = "cmake --build ."
+else
+	-- Default: compile current file with g++
+	local file = vim.fn.expand("%")
+	local output = vim.fn.expand("%:r") -- filename without extension
+	vim.bo.makeprg = string.format("g++ -Wall %s -o %s", file, output)
+end
