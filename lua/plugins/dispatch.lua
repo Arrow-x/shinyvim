47bb378381b49e2182ab6fc58d91bdda692d55ef
Arrow-x <Houssem.kun26@gmail.com>
2025-09-12 12:03:59 +0100

    diapatch keybinds

--- lua/plugins/dispatch.lua
+++ lua/plugins/dispatch.lua
@@ -1,4 +1,13 @@
 return {
 	"tpope/vim-dispatch",
-	cmd = {"Dispatch", "FocusDispatch", "Make"},
+	cmd = { "Dispatch", "FocusDispatch", "Make" },
+	keys = {
+		{
+			"<leader>mm",
+			function()
+				vim.cmd("Make")
+			end,
+			desc = "Run :Make",
+		},
+	},
 }
