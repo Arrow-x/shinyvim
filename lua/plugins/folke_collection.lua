0fe70afdf03c97fbb572a1004a72eb75f079c70b
Arrow-x <Houssem.kun26@gmail.com>
2025-10-26 12:03:51 +0100

    all folke stuff together

--- lua/plugins/folke_collection.lua
+++ lua/plugins/folke_collection.lua
@@ -87,4 +87,24 @@ return {
 			},
 		},
 	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	{
+		"folke/snacks.nvim",
+		---@type snacks.Config
+		opts = {
+			image = {
+				-- your image configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			},
+		},
+	},
 }
